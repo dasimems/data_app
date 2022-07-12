2acf5a528404a36879f56a33d1b5e927af302678
dasimems <isaacseun63@gmail.com>
2022-07-12 11:06:25 -0700

    finished the header section

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,98 @@
-import React from "react";
-import {Row, Col } from "antd"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import {Row, Col, Button } from "antd"
+
+import { BiMenu } from "react-icons/bi"
+import { MdOutlineClose } from "react-icons/md"
+import { Link, useParams } from "react-router-dom"
 
 export default function Header(){
+
+
+    var linkParams = useParams(),
+        headerProps = {
+            activeLink: "home",
+            mobileHeader: false
+        },
+        [headerState, setHeaderState] = useState(headerProps),
+        pageLinks = [
+            {
+                link: "home",
+                label: "Home",
+                borderAvailable: false
+            },
+            {
+                link: "service",
+                label: "Service",
+                borderAvailable: false
+            },
+            {
+                link: "about",
+                label: "About",
+                borderAvailable: false
+            },
+            {
+                link: "price",
+                label: "Price",
+                borderAvailable: false
+            },
+            {
+                link: "testimonial",
+                label: "Testimonial",
+                borderAvailable: false
+            },
+            {
+                link: "faq",
+                label: "FAQ",
+                borderAvailable: false
+            },
+            {
+                link: "login",
+                label: "Sign In",
+                borderAvailable: true
+            }
+        ];
+
+
+    useEffect(()=>{
+
+        var links = linkParams["*"].split("/");
+
+        setHeaderState((prevState)=>{
+
+            return(
+                {
+                    ...prevState,
+                    mobileHeader: false,
+                    activeLink: links[0] === ""? "home": links[0]
+                }
+            );
+
+        });
+
+        // console.log(links[0]);
+
+    }, [linkParams]);
+
+    function changeHeader(){
+
+        setHeaderState((prevState)=>{
+            return(
+                {
+                    ...prevState,
+                    mobileHeader: !headerState.mobileHeader
+                }
+            );
+        })
+    }
+
+    // console.log(headerState);
+
+    
 
     return(
         <>
     
-            <Row justify="space-between">
+            <Row justify="space-between" className="header">
 
                 <Col span={5} push={1}className="header-logo">
 
@@ -15,9 +100,55 @@ export default function Header(){
                     
                 </Col>
 
-                <Col span={10} pull={1} className="header-Links">
-                    this are just the links
+                <Col span={14} lg={{span: 14}} md={{span: 16}} sm={{span: 15}} pull={1} className="header-Links">
+                    
+                    <ul className="link-contents">
+
+                        {pageLinks.map((links, linkKey)=>{
+
+                            // console.log(linkKey)
+
+                            return(
+                                <li key={linkKey} id={headerState.activeLink === links.link? "active-link": "non-active-link"} >
+
+                                    <Link className={links.borderAvailable? "border-link": "non-border-link"} to={`/${links.link}`}>{links.label}</Link>
+
+                                </li>
+                            );
+
+                        })}
+                        
+                    </ul>
+                    
                 </Col>
+
+                
+
+                <Col span={12} pull={1} className="mobile-btn">
+                    <Button className="open-close-button" onClick={changeHeader}>{headerState.mobileHeader? <MdOutlineClose /> : <BiMenu />}</Button>
+                </Col>
+
+                <div className="side-nav" style={{right: headerState.mobileHeader? "0px": "-400px"}}>
+
+                    <ul className="link-contents">
+
+                        {pageLinks.map((links, linkKey)=>{
+
+                            // console.log(linkKey)
+
+                            return(
+                                <li key={linkKey} id={headerState.activeLink === links.link? "active-link": "non-active-link"} >
+
+                                    <Link className={links.borderAvailable? "border-link": "non-border-link"} to={`/${links.link}`}>{links.label}</Link>
+
+                                </li>
+                            );
+
+                        })}
+                        
+                    </ul>
+
+                </div>
 
             </Row>
             
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/styles/index.css';
+import './assets/fonts/font-style.css';
 import 'antd/dist/antd.css';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
 
-      <App />
+      <Routes>
+        
+        <Route path='/*' element={<App />} />
+
+      </Routes>
+
 
     </BrowserRouter>
   </React.StrictMode>
