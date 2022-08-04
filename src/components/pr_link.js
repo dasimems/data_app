51eae2774ceabdb9514027128c7521f41cb0401b
dasimems <isaacseun63@gmail.com>
2022-08-04 17:20:12 -0400

    Updated the check email for reset link component

--- src/components/pr_link.js
+++ src/components/pr_link.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Col } from 'antd'
+import { Button, Col } from 'antd'
 
 import VerifImage from "../assets/images/verification.png"
 
 export default function PRLink() {
   document.title = "Verify Password Reset"
+
+  //code to check if email is available and password reset link has been sent else redirect to default password reset page where the user is required to input his or her email
+
+  function resendMail(){
+
+  }
+
   return (
     <>
     
@@ -13,6 +20,8 @@ export default function PRLink() {
           <img src={VerifImage} alt="email envelope" className="verify-image" />
 
           <p className="verify-text">Please Check your inbox for the verification link sent to your Email</p>
+
+          <Button type="primary" className="redo-button" onClick={resendMail}>Resend link</Button>
 
           
 
